69ea79bba45273e08b95929c9f6e60160d9861f3
Carlos Madero <cmadero08x@gmail.com>
2026-06-17 16:16:02 -0500

    Se sube la base del proyecto en la que se va a trabajar

---
+++ js/app.js
@@ -0,0 +1,8 @@
+import { Navbar } from './components/navbar.js';
+import { Footer } from './components/footer.js';
+
+const navbar = new Navbar();
+navbar.render();
+
+const footer = new Footer();
+footer.render();
