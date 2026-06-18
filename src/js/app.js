11b4281a7b4ddf98303fef96d755cc006665aae5
Carlos Madero <cmadero08x@gmail.com>
2026-06-17 19:14:45 -0500

    se alarga video para que no se note salto de vide cuando se reinicia el loop

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,8 @@
 import { Navbar } from './components/navbar.js';
 import { Footer } from './components/footer.js';
-import { HeroVideo } from './components/hero-video.js';
 
 const navbar = new Navbar();
 navbar.render();
 
 const footer = new Footer();
 footer.render();
-
-const heroVideo = new HeroVideo();
-heroVideo.init();
